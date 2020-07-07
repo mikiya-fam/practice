dd3b8aa548395149888efccb20bc16cebbc2215f
塚本龍幸 <tsukamototatsuyuki@tsukamotoryuukounoMacBook-Pro.local>
2020-07-08 01:36:58 +0900

    add new file

--- startWeb.py
+++ startWeb.py
@@ -17,6 +17,22 @@ def test():
 
     return render_template('schedule/scheduleTop.html',userId=userId,userPassword=userPassword)
 
+@app.route("/edit/")
+def edit():
+    return render_template('schedule/scheduleEdit.html')
+
+@app.route("/editComplete/", methods = ['POST','GET'])
+def editComplete():
+    if request.method == 'GET':
+        time = request.args.get('time', '')
+        eventName = request.args.get('eventName', '')
+        place = request.args.get('place', '')
+        comment = request.args.get('comment', '')
+        participant = request.args.get('participant', '')
+
+    return render_template('schedule/scheduleeditComplete.html',time=time,eventName=eventName,place=place,comment=comment,participant=participant)
+
+
 if __name__ == "__main__":
     # webサーバーの立ち上げ
     app.run()
