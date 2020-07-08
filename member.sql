2fde756eb622f81f3eebd9918dd3f6005ebfa3fa
亀山彰人 <kameyamaakihito@kameyamaakihitonoMacBook-Pro.local>
2020-07-08 23:07:31 +0900

    DDL追加

---
+++ member.sql
@@ -0,0 +1,11 @@
+CREATE TABLE T_MEMBER(
+    id int not null primary key,
+    last_name varchar(10),
+    first_name varchar(10),
+    sex char(1),
+    user_id varchar(10),
+    password varchar(10),
+    create_date timestamp default current_timestamp,
+    update_date timestamp default current_timestamp,
+    delete_flag char(1)
+);
